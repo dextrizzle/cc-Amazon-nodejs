87b2acc291ae475db007552d55f503761240bd69
dextrizzle <alfonzo_01@hotmail.com>
2017-04-26 10:19:32 -0700

    seed questions and install faker

---
+++ seeders/20170426170931-seed-products.js
@@ -0,0 +1,45 @@
+'use strict';
+const M = require('../models/index');
+const Product = M.Product;
+const faker = require('faker');
+
+//Array from can create arrays in a variety of different
+//ways. using it as follows will create an array with
+//100 elements.
+const products = Array
+  .from({length: 50})
+  .map(function(){
+    return Product.create({
+      title:`${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+      description: faker.hacker.phrase(),
+      price: 22.50
+    })
+  })
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+
+    */
+    return Promise.all(products);
+
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
